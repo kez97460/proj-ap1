3f48e666a594326f1fa3f77cc4f6d95168b9cd2f
Kezia Marcou <kez.marcou@gmail.com>
2022-11-25 23:54:41 +0100

    New libraries

---
+++ lib_proj.h
@@ -0,0 +1,11 @@
+/* 
+Auteur : Kezia Marcou 
+Une description de chaque fonction est présente la ligne avant sa définition
+
+>> Pour compiler un programme ( main.c ) qui utilise cette librairie :
+1.  gcc -c lib_proj.c ( pour créer arrays_utils.o )
+2.  gcc -c main.c ( pour créer main.o )
+3.  gcc main.o lib_proj.o -o appli ( pour créer l'executable appli )
+
+>> Lignes de code modifiables :
+*/
