6c3c5e916b5ddccd27eb91f760147ce905d4467b
Kezia Marcou <kez.marcou@gmail.com>
2022-11-26 23:57:55 +0100

    added include guards

--- float_lists_utils.h
+++ float_lists_utils.h
@@ -11,6 +11,11 @@ Une description de chaque fonction est présente la ligne avant sa définition
 - type noeud -> le type de data peut être modifié 
 - merge_list -> la fonction qui définit l'ordre peut être modifiée ( dans le .c , line 49 , chiant mais j'ai pas de meilleure idée , si quelqu'un en a une suis preneur )
 */
+#ifndef FLOAT_LISTS_UTILS_H
+#define FLOAT_LISTS_UTILS_H
+
+
+
 
 typedef struct no 
 {
@@ -34,3 +39,5 @@ float max_of_list( noeud* tete ) ;
 float mediane_of_list( noeud* tete ) ;
 float moyenne_of_list( noeud* tete ) ;
 float ecart_type_of_list( noeud* tete ) ;
+
+#endif
--- lib_proj.h
+++ lib_proj.h
@@ -9,30 +9,36 @@ Une description de chaque fonction est présente la ligne avant sa définition
 
 >> Lignes de code modifiables :
 */
+#ifndef LIB_PROJ_H
+#define LIB_PROJ_H
+
+#include "float_lists_utils.h"
 
 typedef struct 
 {
-float* usage_of_backing_film ; // colonne 7
-float* usage_of_dresser ;
-float* usage_of_polishing_table ;
-float* usage_of_dresser_table ;
-float* pressurized_chamber_pressure ;	
-float* main_outer_air_bag_pressure ;	
-float* center_air_bag_pressure ; 
-float* retainer_ring_pressure ;
-float* ripple_air_bag_pressure ;
-float* usage_of_membrane ;
-float* usage_of_pressurized_sheet ;
-float* slurry_flow_line_a ;	
-float* slurry_flow_line_b ;	
-float* slurry_flow_line_c ;	
-float* wafer_rotation ;	
-float* stage_rotation ;	
-float* head_rotation ;	
-float* dressing_water_status ;	// colonne 24 : 0 ou 1 , mais bon les float c'est plus pratique
-float* edge_air_bag_pressure ; 
+noeud* usage_of_backing_film ; // colonne 7
+noeud* usage_of_dresser ;
+noeud* usage_of_polishing_table ;
+noeud* usage_of_dresser_table ;
+noeud* pressurized_chamber_pressure ;	
+noeud* main_outer_air_bag_pressure ;	
+noeud* center_air_bag_pressure ; 
+noeud* retainer_ring_pressure ;
+noeud* ripple_air_bag_pressure ;
+noeud* usage_of_membrane ;
+noeud* usage_of_pressurized_sheet ;
+noeud* slurry_flow_line_a ;	
+noeud* slurry_flow_line_b ;	
+noeud* slurry_flow_line_c ;	
+noeud* wafer_rotation ;	
+noeud* stage_rotation ;	
+noeud* head_rotation ;	
+noeud* dressing_water_status ;	// colonne 24 : 0 ou 1 
+noeud* edge_air_bag_pressure ; 
 }
-data ;
+full_data ;
+
+#endif
 
 
 
