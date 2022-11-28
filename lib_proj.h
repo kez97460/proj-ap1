5b35a987e3dbcad3fa46a5902961a405c7643aa3
Kezia Marcou <kez.marcou@gmail.com>
2022-11-28 14:45:14 +0100

    debut de qqe chose

--- lib_proj.h
+++ lib_proj.h
@@ -14,30 +14,14 @@ Une description de chaque fonction est présente la ligne avant sa définition
 
 #include "float_lists_utils.h"
 
-// type full_data va contenir toutes les infos utiles du .csv
+
 typedef struct 
 {
-noeud* usage_of_backing_film ; // colonne 7
-noeud* usage_of_dresser ;
-noeud* usage_of_polishing_table ;
-noeud* usage_of_dresser_table ;
-noeud* pressurized_chamber_pressure ;	
-noeud* main_outer_air_bag_pressure ;	
-noeud* center_air_bag_pressure ; 
-noeud* retainer_ring_pressure ;
-noeud* ripple_air_bag_pressure ;
-noeud* usage_of_membrane ;
-noeud* usage_of_pressurized_sheet ;
-noeud* slurry_flow_line_a ;	
-noeud* slurry_flow_line_b ;	
-noeud* slurry_flow_line_c ;	
-noeud* wafer_rotation ;	
-noeud* stage_rotation ;	
-noeud* head_rotation ;	
-noeud* dressing_water_status ;	// colonne 24 : 0 ou 1 
-noeud* edge_air_bag_pressure ; 
+char nom[100] ;
+noeud* data ;
 }
-full_data ;
+colonne ;
+
 
 #endif
 
