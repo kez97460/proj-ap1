87630e822873a31ec5b791cf335c9db0f8a2a703
Kezia Marcou <kez.marcou@gmail.com>
2022-11-29 10:16:36 +0100

    Added read_data ( not tested )

--- lib_proj.c
+++ lib_proj.c
@@ -8,15 +8,31 @@
 colonne* read_data( char* filename )
 {
 char name[100] ;
-int len_colonnes = 19 ;
+int j ;
+int nb_colonnes_total = 25 ;
+int nb_colonnes_utiles = 19 ; 
 FILE* file = fopen(filename,"r") ;
-colonne* res = (colonne*) malloc(len_colonnes*sizeof(colonne)) ; 
-for( int i = 0 ; i<len_colonnes ; i++ )
+colonne* res = (colonne*) malloc(nb_colonnes_utiles*sizeof(colonne)) ; 
+for( int i = 1 ; i<=nb_colonnes_total ; i++ ) // on parcourt la 1ere ligne et initialise les listes
     {
     fscanf(file,"%s",name) ; // les noms des colonnes sont au début du .csv
-    res[i].data = NULL ;
-    strcpy(res[i].nom,name) ;
+    if ( i > 6 ) // condition qui donne les colonnes utiles uniquement ( Cols 7 à 25 -> i>6 )
+        {
+        res[j].data = NULL ;
+        strcpy(res[j].nom,name) ;
+        j++ ;
+        }
     }
-
+float temp ;
+for( int i = 1 ; i<=nb_colonnes_total ; i++ ) // remplissage des listes
+    {
+    fscanf(file,"%f",&temp) ;
+    if ( i > 6 ) // condition qui donne les colonnes utiles uniquement ( Cols 7 à 25 -> i>6 )
+        {
+        res[j].data = add_noeud(res[j].data,temp) ;
+        j++ ;
+        }
+    }
+return res ;
 }
 
